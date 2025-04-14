6d2476a22702c02d22170fe5453a5670fe541e68
DianaZelenska <diastrekoza@gmail.com>
2025-04-14 23:49:09 +0200

    Add files via upload

---
+++ sql/testdaten.sql
@@ -0,0 +1,6 @@
+INSERT INTO kontinent(Name) VALUES ('Afrika'),('Asien'),('Europa'),('Australien'),('Nordamerika'),('Südamerika');
+
+INSERT INTO tierart(Name) VALUES ('Säugetiere'),('Vögel'),('Reptilien'),('Insekten'),('Haustier');
+
+
+
